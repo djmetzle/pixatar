0bc336286bff674e25ab90ae903cabe9599c3a16
metz <daryljmetzler@gmail.com>
2025-04-12 12:47:56 -0700

    Use new data for generator

--- src/generator.rs
+++ src/generator.rs
@@ -13,29 +13,12 @@ const SCALE: u32 = 32;
 
 mod bytes;
 
-fn get_image_dimensions(str: &String) -> (u32, u32) {
-    let raw_bytes = str.as_bytes();
-    let len = raw_bytes.len() as u32;
-    return (len * SCALE, 8 * SCALE);
-}
+type PixelValues = (u8, u8, u8, u8);
 
-fn pixel_bytes(char: u8, bit: u8, color: &Hsl, bg: u8, opacity: u8) -> (u8, u8, u8, u8) {
-    let r = color.get_red() as u8;
-    let g = color.get_green() as u8;
-    let b = color.get_blue() as u8;
-    let mask = 1 << bit;
-    if char & mask > 0 {
-        return (r, g, b, 255);
-    }
-    return (bg, bg, bg, opacity);
-}
-
-fn get_png_bytes(str: &String, spec: &Spec) -> Vec<u8> {
-    let raw_bytes = str.as_bytes();
-    let mut data: Vec<u8> = Vec::new();
+fn get_color_values(spec: &Spec) -> (PixelValues, PixelValues) {
     let hue = spec.hue;
     let color = Hsl::from(hue as f32, 100.0, 50.0);
-    let background: u8 = match spec.bg {
+    let background_color: u8 = match spec.bg {
         Background::Black => 0,
         Background::White => 255,
     };
@@ -44,24 +27,41 @@ fn get_png_bytes(str: &String, spec: &Spec) -> Vec<u8> {
         Opacity::Transparent => 0,
     };
 
-    let columns = 0..8;
-    let bytes_len = raw_bytes.len();
+    let foreground = (
+        color.get_red() as u8,
+        color.get_green() as u8,
+        color.get_blue() as u8,
+        255,
+    );
 
-    columns.for_each(|bit| {
-        for _bit_i in 0..SCALE {
-            let rows = 0..bytes_len;
-            rows.for_each(|char_i| {
-                let char = raw_bytes[char_i];
-                for _char_i in 0..SCALE {
-                    let (r, g, b, a) = pixel_bytes(char, bit, &color, background, opacity);
+    let background = (
+        background_color,
+        background_color,
+        background_color,
+        opacity,
+    );
+
+    return (foreground, background);
+}
+
+fn get_png_bytes(str: &String, spec: &Spec, gen_bytes: bytes::Bytes) -> Vec<u8> {
+    let mut data: Vec<u8> = Vec::new();
+
+    let (foreground, background) = get_color_values(spec);
+
+    for row in gen_bytes {
+        for ri in 0..SCALE {
+            for column in row.clone() {
+                for ci in 0..SCALE {
+                    let (r, g, b, a) = if column { foreground } else { background };
                     data.push(r);
                     data.push(g);
                     data.push(b);
                     data.push(a);
                 }
-            });
+            }
         }
-    });
+    }
 
     return data;
 }
@@ -88,7 +88,7 @@ fn generate_image(bytes: &mut Vec<u8>, str: &String, spec: &Spec, gen_bytes: byt
 
     let mut writer = encoder.write_header().unwrap();
 
-    let data = get_png_bytes(str, spec);
+    let data = get_png_bytes(str, spec, gen_bytes);
 
     writer.write_image_data(&data).unwrap();
 }
@@ -98,7 +98,7 @@ fn get_data_url(string: String, spec: Spec) -> String {
         return String::from("");
     }
     let mut bytes: Vec<u8> = Vec::new();
-    let gen_bytes = bytes::Bytes::new(string.clone(), &spec.orient, &spec.ordering);
+    let mut gen_bytes = bytes::Bytes::new(string.clone(), &spec.orient, &spec.ordering);
 
     generate_image(&mut bytes, &string, &spec, gen_bytes);
 
